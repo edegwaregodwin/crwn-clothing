c2658be8676d051471fdee64dd1e2bd27a84203c
edegwaregodwin <edegwaregodwin@gmail.com>
2023-04-09 15:25:29 +0100

    replaced class componenets with hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {createStructuredSelector} from 'reselect';
+import { createStructuredSelector } from 'reselect';
 
-import './App.css'; 
+import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -14,53 +14,48 @@ import { authOnSnapshot, authOnAuthStateChanged, createUserProfileDocument } fro
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
-class App extends React.Component {
+const App = ({currentUser, setCurrentUser}) => {
+  let unsubscribeFromAuth = null;
 
-  unsubscribeFromAuth = null;
+  unsubscribeFromAuth = () => {
+    authOnAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-  componentDidMount() {
-    const {setCurrentUser} = this.props;
-    this.unsubscribeFromAuth = () => {
-      authOnAuthStateChanged(async (userAuth) => {
-        if (userAuth) {
-          const userRef = await createUserProfileDocument(userAuth);
-
-          authOnSnapshot(userRef, (snapShot) => {
-            setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-            });
+        authOnSnapshot(userRef, (snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
-        }
+        });
+      }
 
-        setCurrentUser(userAuth);
-      })
-    }
+      setCurrentUser(userAuth);
+    })
   }
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
+  useEffect(() => {
+    unsubscribeFromAuth();
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/shop/*' element={<ShopPage />} />
-          <Route path='/signin' 
-            element={
-              this.props.currentUser 
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop/*' element={<ShopPage />} />
+        <Route path='/signin'
+          element={
+            currentUser
               ? (<Navigate to='/' />)
               : (<SignInandSignUpPage />)
-              } />
-          <Route exact path='/checkout' element={<CheckoutPage />} />
-        </Routes>
-      </div>
-    );
-  }
+          } />
+        <Route exact path='/checkout' element={<CheckoutPage />} />
+      </Routes>
+    </div>
+  );
 }
+
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -7,32 +7,28 @@ import { authCreateUserWithEmailAndPassword, createUserProfileDocument } from '.
 
 import './sign-up.styles.scss';
 
-class Signup extends React.Component {
-    constructor() {
-        super();
+const SignUp = () => {
+    const [userCredentials, setUserCredentials] = useState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
 
-        this.state = {
-            displayName: '',
-            email: '',
-            password: '',
-            confirmPassword: ''
-        }
-    }
+    const { displayName, email, password, confirmPassword } = userCredentials;
 
-    handleSubmit = async event => {
+    const handleSubmit = async event => {
         event.preventDefault();
 
-        const { displayName, email, password, confirmPassword } = this.state;
-
-        if(password !== confirmPassword) {
+        if (password !== confirmPassword) {
             alert("passwords don't match");
             return;
         }
 
         try {
             const user = await authCreateUserWithEmailAndPassword(email, password, displayName);
-            await createUserProfileDocument(user,  { displayName });
-            this.setState({ 
+            await createUserProfileDocument(user, { displayName });
+            setUserCredentials({
                 displayName: '',
                 email: '',
                 password: '',
@@ -43,57 +39,53 @@ class Signup extends React.Component {
         }
     }
 
-    handleChange = event => {
+    const handleChange = event => {
         const { name, value } = event.target;
 
-        this.setState({ [name]: value })
+        setUserCredentials({ ...userCredentials, [name]: value })
     }
 
-    render() {
-        const { displayName, email, password, confirmPassword } = this.state;
-        return (
-            <div className='sign-up'>
-                <h2 className='title'>I do not have an account</h2>
-                <span>Sign up with your email and password</span>
-                <form className='sign-up-form' onSubmit={this.handleSubmit}>
-                    <FormInput
-                        type='text'
-                        name='displayName'
-                        value={displayName}
-                        onChange={this.handleChange}
-                        label='Display Name'
-                        required
-                    />
-                    <FormInput
-                        type='email'
-                        name='email'
-                        value={email}
-                        onChange={this.handleChange}
-                        label='Email'
-                        required
-                    />
-                    <FormInput
-                        type='password'
-                        name='password'
-                        value={password}
-                        onChange={this.handleChange}
-                        label='Password'
-                        required
-                    />
-                    <FormInput
-                        type='password'
-                        name='confirmPassword'
-                        value={confirmPassword}
-                        onChange={this.handleChange}
-                        label='Confirm Password'
-                        required
-                    />
-                    <CustomButton type='submit'>SIGN UP</CustomButton>
-                </form>
-            </div>
-        )
-    }
-
+    return (
+        <div className='sign-up'>
+            <h2 className='title'>I do not have an account</h2>
+            <span>Sign up with your email and password</span>
+            <form className='sign-up-form' onSubmit={handleSubmit}>
+                <FormInput
+                    type='text'
+                    name='displayName'
+                    value={displayName}
+                    onChange={handleChange}
+                    label='Display Name'
+                    required
+                />
+                <FormInput
+                    type='email'
+                    name='email'
+                    value={email}
+                    onChange={handleChange}
+                    label='Email'
+                    required
+                />
+                <FormInput
+                    type='password'
+                    name='password'
+                    value={password}
+                    onChange={handleChange}
+                    label='Password'
+                    required
+                />
+                <FormInput
+                    type='password'
+                    name='confirmPassword'
+                    value={confirmPassword}
+                    onChange={handleChange}
+                    label='Confirm Password'
+                    required
+                />
+                <CustomButton type='submit'>SIGN UP</CustomButton>
+            </form>
+        </div>
+    )
 }
 
-export default Signup;
+export default SignUp;
